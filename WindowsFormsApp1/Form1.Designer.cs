4050818511b363e5f9df7995a8d59db3c58fa726
Fraikly <Frolova55@outlook.com>
2023-10-15 15:33:01 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 26);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(751, 373);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(842, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(275, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(842, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Отобразить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(842, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выберите таблицу";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1342, 514);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Работа с бд";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        NpgsqlDataAdapter adapter;
+        DataTable table = new DataTable();
+
+        string[] comboBoxTables = {
+            "Роли",
+            "Пользователи",
+            "Типы товаров",
+            "Товары",
+            "orders",
+            "order_products"
+        };
+
+        string[] tables = {
+            "roles",
+            "users",
+            "product_types",
+            "products",
+            "orders",
+            "order_products"
+        };
+
+        ToolStripMenuItem deleteMenuItem = new ToolStripMenuItem("Удалить выбранное");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.AddRange(comboBoxTables);
+            comboBox1.SelectedIndex = 0;
+
+            contextMenuStrip1.Items.AddRange(new[] { deleteMenuItem });
+            dataGridView1.ContextMenuStrip = contextMenuStrip1;
+
+            deleteMenuItem.Click += deleteMenuItem_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tableNumber = comboBox1.SelectedIndex;
+
+            string connString = "Host=127.0.0.1;Username=postgres;Password=root;Database=jewlery_shop";
+            NpgsqlConnection conn = new NpgsqlConnection(connString);
+            conn.Open();
+
+            string cmnd = $"SELECT * from {tables[tableNumber]}";
+            adapter = new NpgsqlDataAdapter(cmnd, conn);
+
+            DataSet ds = new DataSet();
+            ds.Reset();
+            adapter.Fill(ds);
+
+            table = ds.Tables[0];
+            dataGridView1.DataSource = table;
+
+            conn.Close();
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            this.saveData();
+        }
+
+        private void deleteMenuItem_Click(object sender, EventArgs e)
+        {
+            int row = dataGridView1.Rows.GetFirstRow(DataGridViewElementStates.Selected);
+            dataGridView1.Rows.RemoveAt(row);
+
+            this.saveData();
+        }
+
+        private void saveData()
+        {
+            try
+            {
+                NpgsqlCommandBuilder scb = new NpgsqlCommandBuilder(adapter);
+                adapter.Update(table);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
